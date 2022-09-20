fd3e02bc2122e1c95044ad2a85e236c1eea08fc5
Zeeshan <zee_amd@rediff.com>
2022-09-20 05:34:15 +0000

    add acc dev-one

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,27 @@
 module "sandbox" {
   source = "./modules/aft-account-request"
 
-#  control_tower_parameters = {
-#    AccountEmail              = "<ACCOUNT EMAIL>"
-#    AccountName               = "sandbox-aft"
-#    ManagedOrganizationalUnit = "Learn AFT"
-#    SSOUserEmail              = "<SSO EMAIL>"
-#    SSOUserFirstName          = "Sandbox"
-#    SSOUserLastName           = "AFT"
-#  }
-#
-#  account_tags = {
-#    "Learn Tutorial" = "AFT"
-#  }
-#
-#  change_management_parameters = {
-#    change_requested_by = "HashiCorp Learn"
-#    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-#  }
-#
-#  custom_fields = {
-#    group = "non-prod"
-#  }
-#
-#  account_customizations_name = "sandbox"
+  control_tower_parameters = {
+    AccountEmail              = "devacc2009@gmail.com"
+    AccountName               = "dev-one"
+    ManagedOrganizationalUnit = "DEV"
+    SSOUserEmail              = "devacc2009@gmail.com"
+    SSOUserFirstName          = "dev"
+    SSOUserLastName           = "acc"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "devone"
 }
