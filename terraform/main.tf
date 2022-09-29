166b1c8c58cc97a7a41ca644ecd44138ff8f47d8
Zeeshan <zee_amd@rediff.com>
2022-09-29 08:18:34 +0000

    test vpc status in new request

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,3 +28,30 @@ module "sandbox" {
 
   account_customizations_name = "devaccone"
 }
+
+module "sandbox2" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "devacc2909@gmail.com"
+    AccountName               = "devacctwo"
+    ManagedOrganizationalUnit = "DEV"
+    SSOUserEmail              = "devacc9002@gmail.com"
+    SSOUserFirstName          = "dev"
+    SSOUserLastName           = "acc"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+    "Owner" = "CT2"
+    "Environment" = "DEV"
+    "Cost Center" = "CT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  account_customizations_name = "devacctwo"
+}
